fea6e7b858788848fef34f8713ea3ef1741b2593
luminousbinary <aoltolacab@gmail.com>
2025-05-30 03:59:03 +0100

    feat: post redesign

--- components/block/post.tsx
+++ components/block/post.tsx
@@ -1,6 +1,5 @@
-
+import Image from "next/image";
 import Link from "next/link";
-
 
 export type Post = {
   id: string;
@@ -13,17 +12,33 @@ export type Post = {
 };
 
 export default function Post({ post }: { post: Post }) {
-
   return (
     <Link href={`/feed/${post.id}`} className="no-underline">
-    <div  className="post-card p-3 text-black">
-      <h2>{post.title}</h2>
-      <div className="flex">
-        <h4>By {post.author.name}</h4>
-        <h4>| {post.createdAt}</h4>
+
+
+      <div
+        key={post.id}
+        className="border rounded-xl shadow-sm p-4 flex gap-4 items-start"
+      >
+        <Image src={"/default_logo.png"}
+          alt={post.title}
+          width={80}
+          height={40}
+          className="rounded justify-center items-center mb-6"
+        />
+        <div>
+          <h2 className="text-xl font-semibold">{post.title}</h2>
+          <p className="text-gray-600 text-sm mb-1">
+            By {post.author.name} • {new Date(post.createdAt).toDateString()}
+          </p>
+          <p className="text-gray-800">
+            {post.content.length > 100
+              ? post.content.slice(0, 100) + "..."
+              : post.content}
+          </p>
+        </div>
       </div>
-      <p > {post.content} </p>
-    </div>
-    </Link> 
+
+    </Link>
   );
 }
